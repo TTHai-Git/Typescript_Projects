b4e036ecfddd99048f464d2a64ea93152c59c05b
TTHai-Git <spaceboy534@gmail.com>
2025-05-05 14:10:07 +0700

    Fix deploy Nodejs App.

--- PetShop/server/server.js
+++ PetShop/server/server.js
@@ -20,8 +20,15 @@ dotenv.config();
 
 const app = express();
 
+// Connect to MongoDB (run once)
+connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((error) =>
+    console.error("Error connecting to MongoDB:", error.message)
+  );
+
 // Middleware
-app.use(urlencoded({ extended: false })); // request with form data
+app.use(urlencoded({ extended: false }));
 app.use(cors());
 app.use(json());
 
@@ -38,14 +45,5 @@ app.use("/v1/products", productRoutes);
 app.use("/v1/breeds", breedRoutes);
 app.use("/v1/favorites", favoriteRoutes);
 
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-// Connect to MongoDB
-connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB!"))
-  .catch((error) =>
-    console.error("Error connecting to MongoDB:", error.message)
-  );
+// Export for Vercel
+export default app;
