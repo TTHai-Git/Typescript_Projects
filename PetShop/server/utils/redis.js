34f360fc01a56cd9526c775c9c480a510595513d
Trịnh Thanh Hải <110751427+TTHai-Git@users.noreply.github.com>
2025-10-25 14:38:09 +0700

    Fix timeout when use redis on server render.

--- PetShop/server/utils/redis.js
+++ PetShop/server/utils/redis.js
@@ -1,7 +1,11 @@
 import Redis from "ioredis";
 import "../config/dotenv.config.js"; // loads .env
 
-const redis = new Redis(process.env.REDIS_URL, {
+const redis = new Redis({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
   tls: {
     rejectUnauthorized: false, // required for Render Redis
   },
