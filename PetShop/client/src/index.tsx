07c1d16862d2c76aa1ae30d3ce1ee2d4e3256cd4
Trịnh Thanh Hải <110751427+TTHai-Git@users.noreply.github.com>
2026-03-26 18:56:12 +0700

    Update: add analytics vercell.

--- PetShop/client/src/index.tsx
+++ PetShop/client/src/index.tsx
@@ -1,12 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import App from './App'
 import "./i18n"; // 👈 Import here
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
